c6fb24cbb246bca884687d81eb9314bd87c5cd13
Eng\waleed elshfey <waleed@gmail.com>
2024-08-09 20:07:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let sora = document.querySelector(".main-sora");
+let container = document.querySelector(".container");
+function change(phone){
+sora.src = phone;
+}
+
+
+function changeContainer(color){
+    container.style.backgroundColor = color;
+    }
